6a2e0e3493cb65f25ae22d1cd283131c8271983c
Michal Cwienczek <michael.cwienczek@gmail.com>
2019-03-15 19:35:47 +0100

    Reverse proxy working

--- start_proxy.sh
+++ start_proxy.sh
@@ -2,8 +2,6 @@
 
 docker network create --driver bridge nginx_multidocker
 
-docker run --network=nginx_multidocker -itd --name=reverseproxy --volume ${PWD}/reverse_proxy.conf:/etc/nginx/nginx.conf  -p 8080:8080 nginx
-
 docker run --network=nginx_multidocker \
  -itd \
  --name=service1 \
@@ -15,3 +13,8 @@ docker run --network=nginx_multidocker \
  --name=service2 \
  --volume ${PWD}/service2.conf:/etc/nginx/nginx.conf \
  --volume ${PWD}/index2.html:/usr/share/nginx/html/index.html nginx
+
+# before starting reverse proxy the other containers must already reply on http
+sleep 5
+
+docker run --network=nginx_multidocker -itd --name=reverseproxy --volume ${PWD}/reverse_proxy.conf:/etc/nginx/nginx.conf  -p 8080:8080 nginx
---
+++ stop_proxy.sh
@@ -0,0 +1,6 @@
+docker kill reverseproxy;
+docker rm reverseproxy;
+docker kill service1;
+docker rm service1;
+docker kill service2;
+docker rm service2;
